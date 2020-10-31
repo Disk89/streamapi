296ace513a51a20ac2c6aa19a4cdfca2385a0207
d-isk <d@isk.ru>
2020-10-31 18:45:43 +0300

    comment +

--- src/main/java/org/damir/streamapi/StreamApiApp.java
+++ src/main/java/org/damir/streamapi/StreamApiApp.java
@@ -1,26 +1,30 @@
 package org.damir.streamapi;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamApiApp {
     public static void main(String[] args) {
-        String str = "Чтобы начать изучение Java Stream API, разберём основные определения. В языке Java есть понятие\n" +
-                "потоков, но ни классы InputStream (поток ввода) и OutputStream (поток вывода), ни Thread (поток\n" +
-                "исполнения) не имеют ничего общего с новшеством Java 8 — Stream API. Stream API работает не с\n" +
-                "потоком в прямом смысле слова, а с цепочкой функций, вызываемых из самих себя. Он обеспечивает\n" +
-                "функциональное программирование в Java 8. Поток — это последовательность элементов и функций,\n" +
-                "которые поддерживают различные виды операций. Чтобы не путаться в понятиях, обозначим Stream\n" +
-                "API как стрим.";
+        String str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
+                "labore et dolore magna aliqua. A diam sollicitudin tempor id eu nisl nunc mi ipsum. Consequat " +
+                "interdum varius sit amet mattis. Diam vel quam elementum pulvinar. Urna cursus eget " +
+                "nunc scelerisque. Diam maecenas ultricies mi eget mauris pharetra et ultrices." +
+                "Convallis convallis tellus id interdum velit laoreet id donec. Est placerat" +
+                "in egestas erat. Tempus iaculis urna id volutpat lacus laoreet non." +
+                "Morbi tempus iaculis urna id. Libero id faucibus nisl tincidunt eget nullam non nisi." +
+                "Pulvinar proin gravida hendrerit lectus. Vitae et leo duis ut diam quam.";
+
         str = str.replaceAll("\\p{Punct}","");
         String[] strings = str.toLowerCase().split(" ");
+        String word = Arrays.stream(strings).filter((n)-> n.length() > 2)
+                      .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                      .entrySet().stream()
+                      .max(Comparator.comparingLong(e -> e.getValue()))
+                      .get().getKey();
 
-        HashMap<String, Integer> findMacCount = new HashMap<>();
-        for (int i = 0; i< strings.length; i++){
-            findMacCount.put(strings[i], findMacCount.getOrDefault(strings[i], 0) + 1);
-        }
-        System.out.println(findMacCount);
-
+        System.out.println(word);
     }
 }
